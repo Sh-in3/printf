1ae3ce390429779e31a8a799c1372941ed0d346b
Sh-in3 <majorshine14@gmail.com>
2023-10-16 07:25:47 -0700

    printf

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # printf
+
+### A function to do what printf does i.e print a string to the standard output
---
+++ _printf.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ * _printf - a function that does what printf does
+ *
+ * @format: pointer to a string
+ *
+ * Return: to be fixed later to return the correct thing
+ */
+
+int _printf(const char *format, ...)
+{
+	int chara_print = 0;
+
+	va_list args; /* list of arguments (after the string[format]) */
+
+	if (format == NULL)
+		return (-1); /* returns error if string is NULL */
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1); /* if no % sign, write to the standard output */
+			chara_print++;
+		}
+
+		else
+		{
+			format++; /* check the next string if there is a % sign*/
+
+		if (*format == '\0')
+			break;
+
+		else if (*format == 'c')
+		{
+			char c = va_arg(args, int);
+
+			write(1, &c, 1);
+			chara_print++;
+		}
+
+		else if (*format == 's')
+		{
+			char *str = va_arg(args, char*);
+			int str_len = 0;
+
+			while (str[str_len] != '\0')
+				str_len++; /* calculate length of string */
+
+			write(1, str, str_len);
+			chara_print += str_len; /* write to the standard output */
+		}
+
+		else if (*format == '%')
+		{
+			write(1, format, 1);
+			chara_print++;
+		}
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (chara_print);
+}
